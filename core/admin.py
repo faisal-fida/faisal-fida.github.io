d4c1b9605fae687c90bda4eaaffdf5a6f1d7de7d
fasih4444 <FA20-BCS-004@cuiwah.edu.pk>
2022-08-12 12:18:16 +0500

    a

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Student, Teacher, Marksheet, Class, FinalGrades
-
-admin.site.register(Profile)
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Marksheet)
-admin.site.register(Class)
-admin.site.register(FinalGrades)
