77ffe153b0d1fcbdf62e76463664aaec6bfec6e3
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-08-13 09:47:51 +0000

    pipeline

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -15,6 +15,9 @@ import Work1_1 from "../../assets/portfolio/1.1.png";
 import Work1_2 from "../../assets/portfolio/1.2.png";
 import Work1_3 from "../../assets/portfolio/1.3.png";
 import Work1_4 from "../../assets/portfolio/1.4.png";
+import Work2_1 from "../../assets/portfolio/2.1.png";
+import Work2_2 from "../../assets/portfolio/2.2.png";
+import Work2_3 from "../../assets/portfolio/2.3.png";
 
 interface MenuType {
   id: number;
@@ -31,6 +34,9 @@ const imageMap: { [key: string]: string } = {
   "1.2": Work1_2,
   "1.3": Work1_3,
   "1.4": Work1_4,
+  "2.1": Work2_1,
+  "2.2": Work2_2,
+  "2.3": Work2_3,
 };
 
 const ProjectNextArrow = ({ onClick }: { onClick: () => void }) => (
