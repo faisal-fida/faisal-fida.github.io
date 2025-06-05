79dd3dff0c72ee66993a4303b92eaef5f183e9f4
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2025-06-05 09:03:47 +0000

    chore: update dependencies and configuration
    - upgraded vite from version 5.3.4 to 6.3.5 in package.json
    - changed background image path in index.css from './assets/dots-bg.svg' to './assets/others/dots-bg.svg'
    - modified vite.config.ts to explicitly define process.env variables for NODE_ENV and VITE_API_URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": process.env,
+    "process.env": {
+      NODE_ENV: process.env.NODE_ENV,
+      VITE_API_URL: process.env.VITE_API_URL,
+    },
   },
 });
